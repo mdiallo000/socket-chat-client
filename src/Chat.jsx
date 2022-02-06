e8a7a1b88ca2b0c9272f5bd771b433c1caaafd60
mdiallo98 <mamadou1237@gmail.com>
2022-02-06 00:04:22 -0500

    send message logic

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Chat({ socket, username, roomId }) {
-  return <div>Hello</div>;
+  const [currentMessage, SetMessage] = useState("");
+  const SendMessage = async () => {
+    if (currentMessage !== "") {
+      const MessageData = {
+        room: roomId,
+        message: currentMessage,
+        author: username,
+        Time:
+          new Date(Date.now()).getHours() +
+          ":" +
+          new Date(Date.now().getMinutes()),
+      };
+
+      await socket.emit("send_message", MessageData);
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <p>Live</p>
+      </div>
+      <div></div>
+      <div>
+        <input
+          onChange={(e) => {
+            SetMessage(e.target.value);
+          }}
+          type="text"
+          placeholder="Type..."
+        />
+        <button onClick={SendMessage}>&#9658;</button>
+      </div>
+    </div>
+  );
 }
 
 export default Chat;
