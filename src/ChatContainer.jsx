1f0b40cc187da87cca8b4490457202eb5a381a34
mdiallo98 <mamadou1237@gmail.com>
2022-02-05 22:39:09 -0500

    initial buildup

---
+++ src/ChatContainer.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import io from "socket.io-client";
+
+const socket = io.connect("/http://localhost:3001");
+
+function ChatContainer() {
+  const [username, SetUsername] = useState("");
+  const [roomID, SetRoomID] = useState("");
+
+  const joinRoom = () => {
+    if (username !== "" && roomID !== "") {
+    }
+  };
+
+  return (
+    <div>
+      <h3>Join To Chat </h3>
+      <input
+        type="text"
+        placeholder="John..."
+        onChange={(event) => SetUsername(event.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Room ID...."
+        onChange={(event) => SetRoomID(event.target.value)}
+      />
+      <button>Join A Room</button>
+    </div>
+  );
+}
+
+export default ChatContainer;
