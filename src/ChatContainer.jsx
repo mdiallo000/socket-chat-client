eb37edd8ec8065bfbdfe5fa1f5a6326528957a1b
mdiallo98 <mamadou1237@gmail.com>
2022-02-06 22:58:14 -0500

    conditional rendering of chat

--- src/ChatContainer.jsx
+++ src/ChatContainer.jsx
@@ -8,30 +8,35 @@ const socket = io.connect("http://localhost:3001");
 function ChatContainer() {
   const [username, SetUsername] = useState("");
   const [roomID, SetRoomID] = useState("");
-
+  const [showRomm, SetshowRoom] = useState(false);
   const joinRoom = () => {
     if (username !== "" && roomID !== "") {
       socket.emit("join_room", roomID);
+      SetshowRoom(true);
     }
-    // with socket that emit in this situation we are passing
   };
 
   return (
-    <div>
-      <h3>Join To Chat </h3>
-      <input
-        type="text"
-        placeholder="John..."
-        onChange={(event) => SetUsername(event.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="Room ID...."
-        onChange={(event) => SetRoomID(event.target.value)}
-      />
-      <button onClick={joinRoom}>Join A Room</button>
-      <Chat socket={socket} username={username} roomID={roomID} />
-    </div>
+    <>
+      {!showRomm ? (
+        <div className="joinChatContainer">
+          <h3>Join To Chat </h3>
+          <input
+            type="text"
+            placeholder="John..."
+            onChange={(event) => SetUsername(event.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Room ID...."
+            onChange={(event) => SetRoomID(event.target.value)}
+          />
+          <button onClick={joinRoom}>Join A Room</button>
+        </div>
+      ) : (
+        <Chat socket={socket} username={username} roomID={roomID} />
+      )}
+    </>
   );
 }
 
